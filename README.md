fcef9380d11d1642ab6c2ab688fdffa23617d4eb
Jaynti Kanani <jdkanani@gmail.com>
2018-06-12 12:09:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<center>
+  <h1>Matic</h1>
+  [Draft Version 1.0]<br><br>
+  Jaynti Kanani <<a href='mailto:jdkanani@matic.network'>jdkanani@matic.network</a>><br>
+  Sandeep Nailwal <<a href='mailto:sandeep@matic.network'>sandeep@matic.network</a>><br>
+  Anurag Arjun <<a href='mailto:anurag@matic.network'>anurag@matic.network</a>><br>
+  <h2 id="abstract">Abstract</h2>
+</center>
+<div style="text-align: justify; width: 600px; margin: 0 auto;">
+
+## Introduction
---
+++ _book/publish_gitbook.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# install the plugins and build the static site
+./node_modules/.bin/gitbook install && ./node_modules/.bin/gitbook build
+
+# checkout to the gh-pages branch
+git checkout gh-pages
+
+# pull the latest updates
+git pull origin gh-pages --rebase
+
+# copy the static site files into the current directory.
+cp -R _book/* .
+rm -rf package.json package-lock.json publish_gitbook.sh .gitignore
+
+# remove 'node_modules' and '_book' directory
+git clean -fx node_modules
+git clean -fx _book
+
+# add all files
+git add .
+
+# commit
+git commit -a -m "Update docs"
+
+# push to the origin
+git push origin gh-pages
+
+# checkout to the master branch
+git checkout master
